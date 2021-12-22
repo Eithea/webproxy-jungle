4529c0fb715e26351b4f415dbf5c433fb237d410
Eithea <oguraff94@gmail.com>
2021-12-22 10:48:10 +0900

    cgi-bin adder & html

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -24,10 +24,8 @@ int main(void)
   sprintf(content, "QUERY_STRING=%s", buf);
   sprintf(content, "Welcome to add.com: ");
   sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
-          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
   sprintf(content, "%sThanks for visiting!\r\n", content);
-
   /* Generate the HTTP response */
   printf("Connection: close\r\n");
   printf("Content-length: %d\r\n", (int)strlen(content));
